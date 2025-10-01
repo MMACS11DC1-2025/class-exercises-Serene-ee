c5da4d9a7be0575fa65080e8813ea29bb8f0ed8e
Serene-ee <2570413@learn.vsb.bc.ca>
2025-10-01 22:04:48 +0000

    not done

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -6,31 +6,32 @@ You must use user input to add interactivity to the program.
 You must design your algorithm in English first, then translate it to Python code.
 Test as you go! Describe in your comments what steps you took to test your code.
 """
-file = open("responses.csv")
+file = open('2.4/responses.csv')
 junk = file.readline()
 
 print("Please input your name for us to search up your information in the database.")
  
 name = input()
 
-if name in file.readline():
-    my_favourites = file.readline().strip().split(",")
+for line in file:
+    if name in file:
+        my_favourites = file.readline().strip().split(",")
 
-most_sim = 0
-top_friend = ""
+    most_sim = 0
 
 for line in file:
     their_favs = line.strip().split(",")
     their_name = their_favs[1]
 
     common_interest = 0
+    top_friend = ""
 
     for faves in my_favourites:
-        if faves in their_favs:
+        if faves in their_favs:top_friend = ""
             common_interest += 1
 
-    if common_interest > top_score:
+    if common_interest > most_sim:
         top_friend = their_name
-        top_score = common_interest
+        most_sim = common_interest
 
 print(top_friend)
