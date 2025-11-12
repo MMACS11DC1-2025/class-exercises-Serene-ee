7ff9dce58ca8fa85c9e8b1b23840691c7c9dd7b1
Serene-ee <2570413@learn.vsb.bc.ca>
2025-11-12 21:33:35 +0000

    notdone

--- 5.1/image_open.py
+++ 5.1/image_open.py
@@ -1,4 +1,8 @@
 from PIL import Image
 
-image_green = Image.open("CS11/5.1/kid-green.jpg").load()
-image_beach = Image.open("CS11/5.1/beach.jpg").load()
+image_green = Image.open("5.1/kid-green.jpg").load()
+image_beach = Image.open("5.1/beach.jpg").load()
+
+def is_green(r, g, b):
+    if 0 <= r <= 255 and 0 == g and 0 == b:
+        return "green"
