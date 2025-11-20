b3c10fbb13aa725910f494d0d3cb6310bd780c39
Serene-ee <2570413@learn.vsb.bc.ca>
2025-11-20 21:16:16 +0000

    notdone

--- 5.1/practice.py
+++ 5.1/practice.py
@@ -29,7 +29,7 @@ def is_light(r, g, b):
         return True
     elif pixel < 128:
         return False
-'''
+
 from PIL import Image
 
 file = Image.open("jelly_beans.jpg")
@@ -47,7 +47,24 @@ for x in range(width):
         
         if r > 150 and g > 150 and b < 100:
             output_image.putpixel((x, y), (255,255,255))
-            
+'''
+from PIL import Image
+
+file = Image.open("jelly_beans.jpg")
+jb_image = file.load()
+
+width = file.width
+height = file.height
+
+for x in range(width):
+    for y in range(height):
+
+        r = jb_image[x, y][0]
+        g = jb_image[x, y][1]
+        b = jb_image[x, y][2]
+        
+        if r > 150 and g > 150 and b < 100:
+            output_image.putpixel((x, y), (255,255,255))            
 
 
 
