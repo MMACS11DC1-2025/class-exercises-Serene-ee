749bfb28799a2a8bd59e7ffd5865b1c7a2fa9b20
Serene-ee <2570413@learn.vsb.bc.ca>
2025-11-24 21:35:12 +0000

    notdone

--- 5.1/practice.py
+++ 5.1/practice.py
@@ -47,14 +47,21 @@ for x in range(width):
         
         if r > 150 and g > 150 and b < 100:
             output_image.putpixel((x, y), (255,255,255))
+            file.putpixel((x, y), (255,255,255))  
 '''
 from PIL import Image
 
-file = Image.open("jelly_beans.jpg")
+file = Image.open("5.1/jelly_beans.jpg")
 jb_image = file.load()
 
 width = file.width
 height = file.height
+
+yellow_pixels = 0
+red_pixels = 0
+green_pixels = 0
+blue_pixels = 0
+purple_pixels = 0
 
 for x in range(width):
     for y in range(height):
@@ -63,8 +70,31 @@ for x in range(width):
         g = jb_image[x, y][1]
         b = jb_image[x, y][2]
         
+        #yellow
         if r > 150 and g > 150 and b < 100:
-            output_image.putpixel((x, y), (255,255,255))            
+            yellow_pixels += 1
+        #red
+        elif r > 175 and g < 3 and b < 3:   
+            red_pixels += 1
+        #green
+        elif r > 30 and g > 80 and b < 30: 
+            green_pixels += 1
+        #blue
+        elif r < 10 and g < 30 and b > 100: 
+            blue_pixels += 1
+        #purple
+        elif r > 50 and g > 50 and b < 150:    
+            purple_pixels += 1    
 
+percent_yellow = 100*yellow_pixels/(width*height)
+percent_red = 100*red_pixels/(width*height)
+percent_green = 100*green_pixels/(width*height)
+percent_blue = 100*blue_pixels/(width*height)
+percent_purple = 100*purple_pixels/(width*height)
+
+report = "There are {:.2f} percent yellow jellybeans.".format(percent_yellow)
+print(report)
+
+#file.save("output.png", "png")
 
 
