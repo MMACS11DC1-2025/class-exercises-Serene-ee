6a9158bcf8a9afd1f93ead8892969f05759e67ec
Serene-ee <2570413@learn.vsb.bc.ca>
2025-11-25 21:17:26 +0000

    done

--- 5.1/practice.py
+++ 5.1/practice.py
@@ -48,7 +48,8 @@ for x in range(width):
         if r > 150 and g > 150 and b < 100:
             output_image.putpixel((x, y), (255,255,255))
             file.putpixel((x, y), (255,255,255))  
-'''
+
+5.5 p.9
 from PIL import Image
 
 file = Image.open("5.1/jelly_beans.jpg")
@@ -98,3 +99,49 @@ print(report)
 #file.save("output.png", "png")
 
 
+5.5 p.7
+'''
+
+import time
+
+t0 = time.time()
+
+from PIL import Image
+t1 = time.time()
+
+file = Image.open("jelly_beas.jbg")
+jbimg = file.load()
+
+width = file.width
+height = file.height
+
+yellow_pixels = 0
+
+t2 = time.time()
+
+for i in range(width):
+    for j in range (height):
+
+        r = jbimg[i, j][0]
+        g = jbimg[i, j][1]
+        b = jbimg[i, j][2]
+
+        if r > 150 and g >150 and b < 100:
+            yellow_pixels += 1
+t3 = time.time()
+
+print(yellow_pixels)
+print(width*height)
+
+percent_yellow = 100*yellow_pixels/(width*height)
+report = "There are {:.3f} percent yellow jellybeans. ".format(percent_yellow)
+
+print(report)
+
+module_load = t1-t0
+image_open_load = t2-t1
+loop = t3-t2
+entire = t3-t0
+
+timings = "It took {:.2f}s to import PIL, {:.2f}s to load the image, and {:.2f}s to do the loop. All in all it took{:.2f}s.".format(module_load, image_open_load, loop, entire)
+print(timings)
