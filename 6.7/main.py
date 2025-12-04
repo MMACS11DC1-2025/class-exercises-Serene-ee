915fa15688c8543dd6c706c18e2f672d7d73c2a9
Serene-ee <2570413@learn.vsb.bc.ca>
2025-12-04 21:12:35 +0000

    notdone

--- 6.7/main.py
+++ 6.7/main.py
@@ -1,7 +1,12 @@
 from PIL import Image
+files = ["6.7/yellowfish1.jpg", "6.7/yellowfish2.jpg", "6.7/yellowfish3.jpg", "6.7/yellowfish4.jpg", "6.7/yellowfish5.jpg", "6.7/yellowfish6.jpg", 
+         "6.7/redfish1.jpg", "6.7/belugawhale.jpg", "6.7/eagleray.jpg", "6.7/killerwhale.jpg", "6.7/pufferfish.jpg"]
 
-file = Image.open["6.7/yellowfish1.jpg", "6.7/yellowfish2.jpg", "6.7/yellowfish3.jpg", "6.7/yellowfish4.jpg", "6.7/yellowfish5.jpg", "6.7/yellowfish6.jpg", "6.7/redfish1.jpg", "6.7/belugawhale.jpg", "6.7/eagleray.jpg", "6.7/killerwhale.jpg", "6.7/pufferfish.jpg"]
-yf1_image = file.load([0])
+for imgs in files:
+    Image.open[imgs]
+
+
+all_img = {"yf1_image": file.load([0])}
 yf2_image = file.load([1])
 yf3_image = file.load([2])
 yf4_image = file.load([3])
@@ -21,9 +26,9 @@ yellow_pixels = 0
 for x in range(width):
     for y in range(height):
 
-        r = jb_image[x, y][0]
-        g = jb_image[x, y][1]
-        b = jb_image[x, y][2]
+        r = all_img[yf1_image][x, y][0]
+        g = all_img[x, y][1]
+        b = all_img[x, y][2]
         
         #yellow
         if r > 150 and g > 150 and b < 100:
@@ -31,5 +36,5 @@ for x in range(width):
 
 percent_yellow = 100*yellow_pixels/(width*height)
 
-report = "There are {:.2f} percent yellow jellybeans.".format(percent_yellow)
+report = "There are {:.2f} percent colour yellow in this image.".format(percent_yellow)
 print(report)
