c5914cde282c048082bbfeeff2f41892003a8475
Serene-ee <2570413@learn.vsb.bc.ca>
2025-12-05 21:22:34 +0000

    notdone

--- 6.7/main.py
+++ 6.7/main.py
@@ -1,40 +1,62 @@
 from PIL import Image
-files = ["6.7/yellowfish1.jpg", "6.7/yellowfish2.jpg", "6.7/yellowfish3.jpg", "6.7/yellowfish4.jpg", "6.7/yellowfish5.jpg", "6.7/yellowfish6.jpg", 
-         "6.7/redfish1.jpg", "6.7/belugawhale.jpg", "6.7/eagleray.jpg", "6.7/killerwhale.jpg", "6.7/pufferfish.jpg"]
+files = ["6.7/yellowfish1.webp", "6.7/yellowfish2.webp", "6.7/yellowfish3.jpg", "6.7/yellowfish4.jpeg", "6.7/yellowfish5.jpg", "6.7/yellowfish6.jpg", 
+         "6.7/redfish1.jpg", "6.7/belugawhale.jpg", "6.7/eagleray.jpg", "6.7/killerwhale.webp", "6.7/pufferfish.webp"]
 
 for imgs in files:
-    Image.open[imgs]
+    file = Image.open(imgs)
+    loadedFile = file.load()
+
+    width = file.width
+    height = file.height
+
+    yellow_pixels = 0
+
+    for x in range(width):
+        for y in range(height):
+
+            r = loadedFile[x, y][0]
+            g = loadedFile[x, y][1]
+            b = loadedFile[x, y][2]
+            
+            #yellow
+            if r > 150 and g > 150 and b < 100:
+                yellow_pixels += 1   
+
+    percent_yellow = 100*yellow_pixels/(width*height)
+
+    report = "There are {:.2f} percent colour yellow in this image.".format(percent_yellow)
+    print(report)
 
 
-all_img = {"yf1_image": file.load([0])}
-yf2_image = file.load([1])
-yf3_image = file.load([2])
-yf4_image = file.load([3])
-yf5_image = file.load([4])
-yf6_image = file.load([5])
-rf1_image = file.load([6])
-pff1_image = file.load([7])
-kw_image = file.load([8])
-er_image = file.load([9])
-bw_image = file.load([10])
+# all_img = {"yf1_image": file.load([0]), }
+# yf2_image = file.load([1])
+# yf3_image = file.load([2])
+# yf4_image = file.load([3])
+# yf5_image = file.load([4])
+# yf6_image = file.load([5])
+# rf1_image = file.load([6])
+# pff1_image = file.load([7])
+# kw_image = file.load([8])
+# er_image = file.load([9])
+# bw_image = file.load([10])
 
-width = file.width
-height = file.height
+# width = file.width
+# height = file.height
 
-yellow_pixels = 0
+# yellow_pixels = 0
 
-for x in range(width):
-    for y in range(height):
+# for x in range(width):
+#     for y in range(height):
 
-        r = all_img[yf1_image][x, y][0]
-        g = all_img[x, y][1]
-        b = all_img[x, y][2]
+#         r = all_img[yf1_image][x, y][0]
+#         g = all_img[x, y][1]
+#         b = all_img[x, y][2]
         
-        #yellow
-        if r > 150 and g > 150 and b < 100:
-            yellow_pixels += 1   
+#         #yellow
+#         if r > 150 and g > 150 and b < 100:
+#             yellow_pixels += 1   
 
-percent_yellow = 100*yellow_pixels/(width*height)
+# percent_yellow = 100*yellow_pixels/(width*height)
 
-report = "There are {:.2f} percent colour yellow in this image.".format(percent_yellow)
-print(report)
+# report = "There are {:.2f} percent colour yellow in this image.".format(percent_yellow)
+# print(report)
