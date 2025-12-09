b59f4f2ae2669779d285f83be927c30c98379519
Serene-ee <2570413@learn.vsb.bc.ca>
2025-12-09 06:56:29 +0000

    notdone

--- 6.7/README.md
+++ 6.7/README.md
@@ -1,4 +1,5 @@
 '''''
-Our images's theme is creatures under the sea. This program will scan over the given set of images and analyzes how many percent does the
-colour yellow occupied in the image, in order to determine if this image is more or less likely to have a yellow fish in it.
+Our images's theme is creatures under the sea. This program will scan over the given set of images and analyzes how many percent does the colour yellow occupied in the image, in order to determine if this image is more or less likely to have a yellow fish in it. This program will also record the time it takes to do this process, and there will be a report given when the program is done running.
 '''
+
+Our chosen feature will be accurately identified by using the colour(r, g, b) values, it can precisely identify each pixel's colour data by running through the nested loop. Our time countings are also accurate because they rounded up to three decimal places.
--- 6.7/main.py
+++ 6.7/main.py
@@ -1,4 +1,9 @@
+import time
+t0 = time.time()
+
 from PIL import Image
+t1 = time.time()
+
 files = ["6.7/yellowfish1.webp", "6.7/yellowfish2.webp", "6.7/yellowfish3.jpg", "6.7/yellowfish4.jpeg", "6.7/yellowfish5.jpg", "6.7/yellowfish6.jpg", 
          "6.7/redfish1.jpg", "6.7/belugawhale.jpg", "6.7/eagleray.jpg", "6.7/killerwhale.webp", "6.7/pufferfish.webp"]
 
@@ -26,37 +31,11 @@ for imgs in files:
 
     report = "There are {:.2f} percent colour yellow in this image.".format(percent_yellow)
     print(report)
+t2 = time.time()
 
+image_open_load = t1-t0
+loop = t2-t1
+entire = t2-t0
 
-# all_img = {"yf1_image": file.load([0]), }
-# yf2_image = file.load([1])
-# yf3_image = file.load([2])
-# yf4_image = file.load([3])
-# yf5_image = file.load([4])
-# yf6_image = file.load([5])
-# rf1_image = file.load([6])
-# pff1_image = file.load([7])
-# kw_image = file.load([8])
-# er_image = file.load([9])
-# bw_image = file.load([10])
-
-# width = file.width
-# height = file.height
-
-# yellow_pixels = 0
-
-# for x in range(width):
-#     for y in range(height):
-
-#         r = all_img[yf1_image][x, y][0]
-#         g = all_img[x, y][1]
-#         b = all_img[x, y][2]
-        
-#         #yellow
-#         if r > 150 and g > 150 and b < 100:
-#             yellow_pixels += 1   
-
-# percent_yellow = 100*yellow_pixels/(width*height)
-
-# report = "There are {:.2f} percent colour yellow in this image.".format(percent_yellow)
-# print(report)
+timings = "It took {:.3f}s to load the image, and {:.3f}s to do the loop. All in all it took {:.3f}s.".format(image_open_load, loop, entire)
+print(timings)
